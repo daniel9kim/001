90c38063a6757644b1befffb1b44123151057c4a
daniel9kim <withme.jesus@gmail.com>
2024-06-01 16:51:25 +0900

    Create server.js
    001

---
+++ server.js
@@ -0,0 +1,13 @@
+const WebSocket = require('ws');
+
+const server = new WebSocket.Server({ port: 8080 });
+
+alert("server.js");
+
+server.on('connection', ws => {
+    setInterval(() => {
+        ws.send(JSON.stringify({ serverTime: new Date().toISOString() }));
+    }, 1000);
+});
+
+console.log('WebSocket server is running on ws://localhost:8080');
