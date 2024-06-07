423141c095fd6d36f8198cb3b1e7b40d6a5578ee
daniel9kim <withme.jesus@gmail.com>
2024-06-08 06:13:48 +0900

    Update shooting.js

--- shooting.js
+++ shooting.js
@@ -5,19 +5,19 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 class Player {
-    constructor(x, y, width, height, color) {
+    constructor(x, y, width, height, imageSrc) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        this.color = color;
+        this.image = new Image();
+        this.image.src = imageSrc;
         this.speed = 5;
         this.bullets = [];
     }
 
     draw() {
-        context.fillStyle = this.color;
-        context.fillRect(this.x, this.y, this.width, this.height);
+        context.drawImage(this.image, this.x, this.y, this.width, this.height);
     }
 
     move(direction) {
@@ -63,7 +63,36 @@ class Bullet {
     }
 }
 
-const player = new Player(canvas.width / 2 - 25, canvas.height - 60, 50, 50, 'blue');
+class Enemy {
+    constructor(x, y, width, height, imageSrc) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.image = new Image();
+        this.image.src = imageSrc;
+        this.speed = 3;
+    }
+
+    draw() {
+        context.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+
+    update() {
+        this.y += this.speed;
+    }
+}
+
+const player = new Player(canvas.width / 2 - 25, canvas.height - 60, 50, 50, 'player.png');
+const enemies = [];
+let spawnInterval = 2000;
+
+function spawnEnemy() {
+    const enemy = new Enemy(Math.random() * (canvas.width - 50), 0, 50, 50, 'enemy.png');
+    enemies.push(enemy);
+}
+
+setInterval(spawnEnemy, spawnInterval);
 
 function gameLoop() {
     context.clearRect(0, 0, canvas.width, canvas.height);
@@ -75,6 +104,15 @@ function gameLoop() {
 
         if (bullet.y + bullet.height < 0) {
             player.bullets.splice(index, 1);
+        }
+    });
+
+    enemies.forEach((enemy, index) => {
+        enemy.update();
+        enemy.draw();
+
+        if (enemy.y > canvas.height) {
+            enemies.splice(index, 1);
         }
     });
 
@@ -98,6 +136,20 @@ window.addEventListener('keydown', (event) => {
         case ' ':
             player.shoot();
             break;
+    }
+});
+
+// Mobile touch controls
+canvas.addEventListener('touchstart', (event) => {
+    const touchX = event.touches[0].clientX;
+    const touchY = event.touches[0].clientY;
+
+    if (touchY < player.y) {
+        player.shoot();
+    } else if (touchX < player.x) {
+        player.move('left');
+    } else if (touchX > player.x + player.width) {
+        player.move('right');
     }
 });
 
