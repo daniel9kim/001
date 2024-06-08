e4d8d41e0477eaa166a9f07b9ac3f5cc60706b2f
daniel9kim <withme.jesus@gmail.com>
2024-06-08 14:28:56 +0900

    Update shooting.js

--- shooting.js
+++ shooting.js
@@ -17,6 +17,7 @@ canvas.height = window.innerHeight;
 
 let score = 0;
 let gameOver = false;
+let bossSpawned = false;
 
 class Player {
     constructor(x, y, width, height, imageSrc) {
@@ -108,7 +109,6 @@ class Enemy {
 const player = new Player(canvas.width / 2 - 25, canvas.height - 60, 50, 50, 'player.png');
 const enemies = [];
 let spawnInterval = 2000;
-let bossSpawned = false;
 
 function spawnEnemy() {
     let enemy;
@@ -120,7 +120,6 @@ function spawnEnemy() {
         bossSpawned = true;
     } else {
         enemy = new Enemy(Math.random() * (canvas.width - 50), 0, 50, 50, 'enemy.png');
-        bossSpawned = false;
     }
     enemies.push(enemy);
 }
